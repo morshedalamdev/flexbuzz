f9574149e8a9680c775796fc235c027bc1ff0541
Morshed Alam <ma.morshedalampro@gmail.com>
2026-02-24 17:33:23 +0800

    Update root page and global styles
    Update the root page component and global stylesheet, and regenerate Next.js dev artifacts. Changes include edits to client/app/(root)/page.tsx and client/app/globals.css; corresponding .next/dev server chunks, CSS bundles/maps, turbopack cache and trace files were regenerated to reflect the updates.

--- client/.next/dev/server/app/(root)/page.js
+++ client/.next/dev/server/app/(root)/page.js
@@ -1,6 +1,6 @@
 var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/(root)/page.js")
-R.c("server/chunks/ssr/node_modules_next_dist_1366b870._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__dbcb0655._.js")
+R.c("server/chunks/ssr/node_modules_a297a485._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e9b7d9f1._.js")
 R.c("server/chunks/ssr/node_modules_e2d1c5df._.js")
 R.c("server/chunks/ssr/[externals]__e8a2741f._.js")
 R.c("server/chunks/ssr/app_b9b1292a._.js")
--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 
 export default function HomePage() {
   return (
-      <main className="flex w-full min-h-[calc(100dvh-48px)] max-w-3xl mx-auto flex-col items-center justify-between bg-white dark:bg-black">
-        
-      </main>
+    <main className="flex w-full min-h-[calc(100dvh-48px)] max-w-3xl p-3 mx-auto flex-col items-center justify-between bg-white dark:bg-black">
+      <div className="text-sm border rounded-sm p-3 space-y-3 shadow-sm">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam
+          doloremque nemo debitis non harum molestiae, accusantium doloribus
+          adipisci iusto, quis deleniti autem voluptas. Laborum sed deleniti
+          iusto tempora voluptatibus.
+        </p>
+
+        <div className="flex gap-3">
+          <Button variant="outline">
+            <ThumbsUpIcon size={15} />
+            Like (238)
+          </Button>
+          <Button variant="outline">
+            <MessageCircleIcon size={15} />
+            Comments (6)
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
