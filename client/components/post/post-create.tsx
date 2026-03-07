aaabb13f02862585f139cd2ae80adcb79d42db48
Morshed Alam <ma.morshedalampro@gmail.com>
2026-03-07 20:47:00 +0800

    Add post action and update post-create component
    Add a new client action (client/actions/post.ts) to handle post creation and update client/components/post/post-create.tsx to use that action. Development build artifacts and client reference manifest were regenerated (updates to .next dev cache/log and page_client-reference-manifest.js) as part of the rebuild.

--- client/components/post/post-create.tsx
+++ client/components/post/post-create.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { Field, FieldGroup } from "../ui/field";
 import {
   InputGroup,
@@ -5,8 +8,41 @@ import {
   InputGroupButton,
   InputGroupTextarea,
 } from "../ui/input-group";
+import { Spinner } from "../ui/spinner";
+import { useFetcher } from "@/hooks/use-fetcher";
+import { NoteType, StatusType } from "@/lib/types";
+import { useShowToast } from "@/hooks/use-show-toast";
 
 export default function PostCreate({ btnLabel }: { btnLabel: string }) {
+  const { fetcher } = useFetcher<NoteType>("/note");
+  const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetcher({
+        method: "POST",
+        payload: {
+          content,
+        },
+      });
+
+      if (!res.success || !res.data) {
+        useShowToast(StatusType.ERROR, res.message || "Failed to create post");
+      }
+    } catch (error) {
+      useShowToast(
+        StatusType.ERROR,
+        "An error occurred while creating the post",
+      );
+    } finally {
+      setContent("");
+      setIsLoading(false);
+      useShowToast(StatusType.SUCCESS, "Post created successfully");
+    }
+  };
+
   return (
     <FieldGroup>
       <Field>
@@ -14,9 +50,18 @@ export default function PostCreate({ btnLabel }: { btnLabel: string }) {
           <InputGroupTextarea
             id="block-end-textarea"
             placeholder="What's on your mind?"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           />
           <InputGroupAddon align="block-end">
-            <InputGroupButton variant="default" size="sm" className="ml-auto">
+            <InputGroupButton
+              onClick={handleSubmit}
+              disabled={isLoading}
+              variant="default"
+              size="sm"
+              className="ml-auto"
+            >
+              {isLoading ? <Spinner /> : ""}
               {btnLabel}
             </InputGroupButton>
           </InputGroupAddon>
